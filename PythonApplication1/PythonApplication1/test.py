6d9bfe38dd2f3b989332b24ad839cad0314d4fd0
MattSnyder97 <mattsnyder323@gmail.com>
2021-04-08 20:23:52 -0400

    Added Entry Box and Comments to test.py

--- PythonApplication1/PythonApplication1/test.py
+++ PythonApplication1/PythonApplication1/test.py
@@ -17,26 +17,47 @@ def miner_start():
     os.system(
         "ethminer -P stratum1+tcp://0xE3A8103F0c2E17E1AD0bc6935b1D35FB38470C82.deployment@naw-eth.hiveon.net:4444 --report-hashrate")
 
-
+#Setting Up the UI
 root = Tk()
 root.title("Crypto Miner (v0.1)")
 root.resizable(width=False, height=False)
 root.geometry("450x300")
 var = StringVar()
+workerName = StringVar()
 label = Label( root, textvariable=var)
 var.set("Program is turned off.")
 
+#Worker Name Text Display
+workerNameLabel = Label( root, textvariable=workerName)
+workerName.set("Your Miner Name:")
+workerNameLabel.pack()
 
-#Turning the Button On
+#Text Entry Field
+L1 = Label(root, text="Enter Your Name:")
+L1.pack( side = LEFT)
+E1 = Entry(root, bd =1)
+E1.pack(side=LEFT)
+
+
+#Submit
+def submit():
+    input = E1.get()
+    workerName.set("Your Miner Name: " + str(input))
+    print("Worker Name: " + str(input) + "")
+
+
+#Turning the Miner On
 def turnOn():
     var.set("Program is running.")
     thread = threading.Thread(target=miner_start, args=[])
     thread.start()
     print("Bot started mining...")
 
+
 onButton = Button(root, text ="TURN ON", command = turnOn)
 
-#Turning the Button Off
+
+#Turning the Miner Off
 def turnOff():
     var.set("Program is stopped.")
     os.system("TASKKILL /F /IM cmd.exe /T")
@@ -45,7 +66,14 @@ def turnOff():
 
 offButton = Button(root, text ="TURN OFF", command = turnOff)
 
+#Submit Entry Field Text Button
+submitButton = Button(root, text ="SUBMIT", command = submit)
+
+
+################################################################
+#Packing the UI Elements
 onButton.pack()
 offButton.pack()
 label.pack()
+submitButton.pack()
 root.mainloop()
